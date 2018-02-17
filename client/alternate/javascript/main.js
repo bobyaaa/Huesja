97c294535498991d84687e3e0f99fc853d8d91da
AndrewHuang771 <ahuang771@gmail.com>
2018-02-17 17:17:05 -0500

    poo

--- client/alternate/javascript/main.js
+++ client/alternate/javascript/main.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
+	$(".screen2").hide();
+	$(".screenPiano").hide();
+	$(".screenDrums").hide();
+	$(".screenViolin").hide();
+	$(".screenGuitar").hide();
+
+
 	$(".solo").click(function(){
 		$(".screen1").fadeOut(250);
 		$(".screen2").fadeIn(250);
@@ -12,20 +19,22 @@ $(document).ready(function(){
 
 	$("#piano").click(function(){
 		$(".screen2").fadeOut(250);
-		$(".screenPiano").fadeIn(250);
+		setInterval(function(){
+			$(".screenPiano").fadeIn(550);
+		},1000);
 	})
 
-	$(".drums").click(function(){
+	$("#drums").click(function(){
 		$(".screen2").fadeOut(250);
 		$(".screenDrums").fadeIn(250);
 	})
 
-	$(".guitar").click(function(){
+	$("#guitar").click(function(){
 		$(".screen2").fadeOut(250);
 		$(".screenGuitar").fadeIn(250);
 	})
 
-	$(".violin").click(function(){
+	$("#violin").click(function(){
 		$(".screen2").fadeOut(250);
 		$(".screenViolin").fadeIn(250);
 	})
