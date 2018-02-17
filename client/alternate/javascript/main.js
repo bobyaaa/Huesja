f1263e1880e6b4ae0a1a0340db758868e265be44
AndrewHuang771 <ahuang771@gmail.com>
2018-02-17 15:16:29 -0500

    yolo

--- client/alternate/javascript/main.js
+++ client/alternate/javascript/main.js
@@ -3,8 +3,31 @@ $(document).ready(function(){
 		$(".screen1").fadeOut(250);
 		$(".screen2").fadeIn(250);
 	})
+
+
 	$(".multi").click(function(){
 		$(".screen1").fadeOut(250);
 		$(".screen2").fadeIn(250);
 	})
+
+	$("#piano").click(function(){
+		$(".screen2").fadeOut(250);
+		$(".screenPiano").fadeIn(250);
+	})
+
+	$(".drums").click(function(){
+		$(".screen2").fadeOut(250);
+		$(".screenDrums").fadeIn(250);
+	})
+
+	$(".guitar").click(function(){
+		$(".screen2").fadeOut(250);
+		$(".screenGuitar").fadeIn(250);
+	})
+
+	$(".violin").click(function(){
+		$(".screen2").fadeOut(250);
+		$(".screenViolin").fadeIn(250);
+	})
+
 })
