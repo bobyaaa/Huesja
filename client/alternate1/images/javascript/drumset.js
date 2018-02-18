3b410fa332cd230d99fbc5335d063a19ced7008b
AndrewHuang771 <ahuang771@gmail.com>
2018-02-17 20:27:28 -0500

    poop

---
+++ client/alternate1/images/javascript/drumset.js
@@ -0,0 +1,86 @@
+var drums = {};
+var drums2 = {};
+var hihatclosed = new Audio('javascript/Audio/drums/hihatclosed.mp3');
+var hihatopen = new Audio('javascript/Audio/drums/hihatopen.mp3');
+var ridecymbal = new Audio('javascript/Audio/drums/ridecymbal.mp3');
+var crashcymbal = new Audio('javascript/Audio/drums/crashcymbal.mp3');
+var snarelong = new Audio('javascript/Audio/drums/snarelong.mp3');
+var snareshort = new Audio('javascript/Audio/drums/snareshort.mp3');
+var tomtomhigh = new Audio('javascript/Audio/drums/tomtomhigh.mp3');
+var tomtomlow = new Audio('javascript/Audio/drums/tomtomlow.mp3');
+var bass = new Audio('javascript/Audio/drums/bass.mp3');
+
+function startDrums(){
+	drumming.start();
+}
+
+var drumming = {
+    start : function() {
+    	this.interval = setInterval(updateDrums,1);
+        window.addEventListener('keydown', function (e) {
+            drums[e.keyCode] = true;
+            console.log(e.keyCode);
+        })
+        window.addEventListener('keyup', function (e) {
+            drums[e.keyCode] = false;
+            drums2[e.keyCode] = false;
+        })
+    }
+}
+
+function updateDrums() {  
+	if (drums[32]&&!drums2[32]){
+        bass.pause();
+        bass.currentTime = 0;
+        bass.play();
+        drums2[32] = true;
+    }
+    if (drums[82]&&!drums2[82]){
+        crashcymbal.pause();
+        crashcymbal.currentTime = 0;
+        crashcymbal.play();
+        drums2[82] = true;
+    }
+    if (drums[85]&&!drums2[85]){
+        ridecymbal.pause();
+        ridecymbal.currentTime = 0;
+        ridecymbal.play();
+        drums2[85] = true;
+    }
+    if (drums[84]&&!drums2[84]){
+        tomtomlow.pause();
+        tomtomlow.currentTime = 0;
+        tomtomlow.play();
+        drums2[84] = true;
+    }
+    if (drums[89]&&!drums2[89]){
+        tomtomhigh.pause();
+        tomtomhigh.currentTime = 0;
+        tomtomhigh.play();
+        drums2[89] = true;
+    }
+    if (drums[71]&&!drums2[71]){
+        snareshort.pause();
+        snareshort.play();
+        snareshort.currentTime = 0;
+        drums2[71] = true;
+    }
+    if (drums[72]&&!drums2[72]){
+        snarelong.pause();
+        snarelong.currentTime = 0;
+        snarelong.play();
+        drums2[72] = true;
+    }
+    if (drums[87]&&!drums2[87]){
+        if (drums[16]){
+            hihatclosed.pause();
+            hihatclosed.currentTime = 0;
+            hihatclosed.play();
+        }else{
+            hihatopen.pause();
+            hihatopen.currentTime = 0;
+            hihatopen.play();
+        }
+        drums2[87] = true;
+    }
+}
---
+++ client/alternate1/javascript/main.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+	$(".screen2").hide();
+	$(".screen21").hide();
+	$(".screenPiano").hide();
+	$(".screenDrums").hide();
+	$(".screenViolin").hide();
+	$(".screenGuitar").hide();
+
+	$(".solo").click(function(){
+		$(".screen1").fadeOut(250);
+		$(".screen2").fadeIn(250);
+	})
+
+
+	$(".multi").click(function(){
+		$(".screen1").fadeOut(250);
+		$(".screen21").fadeIn(250);
+	})
+
+	$("#piano").click(function(){
+		$(".screen2").fadeOut(250);
+		setInterval(function(){
+			$(".screenPiano").fadeIn(550);
+		},1000);
+	})
+
+	$("#drums").click(function(){
+		$(".screen2").fadeOut(250);
+		$(".screenDrums").fadeIn(250);
+	})
+
+	$("#guitar").click(function(){
+		$(".screen2").fadeOut(250);
+		$(".screenGuitar").fadeIn(250);
+	})
+
+	$("#violin").click(function(){
+		$(".screen2").fadeOut(250);
+		$(".screenViolin").fadeIn(250);
+	})
+
+})
+
+
+var C4=function(){
+	
+}
